20b9d9e37ce4ab3e600bd7fac62d908ba26faed5
Omar Andres Ramirez Rodriguez <omarramirez5876@gmail.com>
2021-10-10 16:22:21 -0500

    roles

--- Tienda-virtual/src/App.jsx
+++ Tienda-virtual/src/App.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';  
 import Login from './pages/login';
-<<<<<<< HEAD
 import ModuloProducto from "./pages/ModuloProducto";
-
-
-=======
 import { Roles } from './pages/roles';
->>>>>>> e4230332e29b8c68d21a627c3790271764d00e38
+
 
 
 
@@ -16,18 +12,9 @@ function App() {
     <div className='App' >
       <Router>
         <Switch>
-<<<<<<< HEAD
-          <Route path='/'>
-            <Login/>
-          </Route>
-
-          <Route path="/producto" exact>
-           <ModuloProducto />
-          </Route>
-=======
+          <Route path="/producto" exact><ModuloProducto /></Route>
           <Route path='/'><Login/></Route>
           <Route path='/roles'><Roles/></Route>
->>>>>>> e4230332e29b8c68d21a627c3790271764d00e38
         </Switch>
       </Router>
       
